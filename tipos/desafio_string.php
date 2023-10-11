f7dd5cbb31a6dddad9e61888cb63928c53ff779f
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-10-10 23:59:42 -0300

    inicia repositorio
    Seções "Básico" e "Tipos" concluídas.

---
+++ tipos/desafio_string.php
@@ -0,0 +1,11 @@
+<div class="titulo">Desafio String</div>
+
+<?php
+
+// Enunciado:
+// Avaliando os métodos da documentação da string,
+// qual o método em que a posição do texto 'abc'
+// na string '!AbcaBcabc' retorna 1?
+
+// https://www.php.net/manual/en/function.stripos.php
+echo stripos('!AbcaBcabc', 'abc');
---
+++ tipos/int.php
@@ -0,0 +1,15 @@
+<div class="titulo">Tipo Inteiro</div>
+
+<?php
+echo 11;
+echo '<br>';
+
+var_dump(11);
+echo '<br>';
+
+echo PHP_INT_MAX, '<br>';
+echo PHP_INT_MIN, '<br>';
+
+echo 011, '<br>'; // Base octal
+echo 0b110_00111001, '<br>'; // Base binária
+echo 0x117_acf0, '<br>'; // Base hexadecimal
